4773eed1b25818fd15075916e409b5a11e5ea3bd
gvjacob <gvjacob@outlook.com>
2021-03-05 19:20:44 -0500

    preview for scene

--- studio/schemas/scene.js
+++ studio/schemas/scene.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Touchpoints from '../components/Touchpoints';
 
 export default {
@@ -6,9 +7,16 @@ export default {
   type: 'object',
   fields: [
     {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
       title: 'Image',
       name: 'image',
       type: 'richImage',
+      validation: (Rule) => Rule.required(),
     },
     {
       title: 'Touchpoints',
@@ -24,4 +32,18 @@ export default {
       of: [{ type: 'string' }],
     },
   ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'image.src.asset.url',
+    },
+
+    prepare({ title, media }) {
+      return {
+        title: title || '',
+        media: media ? <img src={media} /> : '',
+      };
+    },
+  },
 };
