56c0ed7736a6e5e416eb49698fd6e6259d405e5b
gvjacob <gvjacob@outlook.com>
2021-03-23 21:18:18 -0400

    slug field in studio schema

--- studio/schemas/studio.js
+++ studio/schemas/studio.js
@@ -1,3 +1,5 @@
+import { required } from '../utils/validation';
+
 export default {
   title: 'Studio',
   name: 'studio',
@@ -8,6 +10,16 @@ export default {
       name: 'title',
       description: 'Name of this studio.',
       type: 'string',
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      description: 'Recommended: Click on Generate',
+      validation: required,
+      options: {
+        source: 'title',
+      },
     },
     {
       title: 'Description',
