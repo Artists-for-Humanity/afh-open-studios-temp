60501a74fff42bb3ebcc14c81c6b531f4ab0ce0c
gvjacob <gvjacob@outlook.com>
2021-03-28 12:16:31 -0400

    footer styles

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 
 import { RichText, Link } from '@components';
 import s from './styles.module.scss';
@@ -12,20 +13,30 @@ const Footer = ({
   socialMediaLinks,
 }) => {
   return (
-    <footer>
-      <RichText blocks={about} />
-      <RichText blocks={contact} />
-      <p>{copyright}</p>
-      <Link href={ctaLink.url}>{ctaLink.text}</Link>
-      <div>
-        <p>Follow Us</p>
-        <ul>
-          {socialMediaLinks.map(({ text, url }) => (
-            <li>
-              <Link href={url}>{text}</Link>
-            </li>
-          ))}
-        </ul>
+    <footer className={cn(s.container, className)}>
+      <div className={s.content}>
+        <RichText className={s.about} blocks={about} />
+        <Link className={s.cta} href={ctaLink.url}>
+          {ctaLink.text}
+        </Link>
+      </div>
+
+      <div className={s.information}>
+        <div className={s.contact}>
+          <RichText blocks={contact} />
+          <p>{copyright}</p>
+        </div>
+
+        <div className={s.social}>
+          <p>Follow Us</p>
+          <ul>
+            {socialMediaLinks.map(({ text, url }) => (
+              <li>
+                <Link href={url}>{text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
