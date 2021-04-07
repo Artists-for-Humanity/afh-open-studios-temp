19ccf0194e6bd6f2ac664a579fb28d4159472492
gvjacob <gvjacob@outlook.com>
2021-04-06 23:25:29 -0400

    studio sidebar aside

--- web/src/components/StudiosSidebar/index.js
+++ web/src/components/StudiosSidebar/index.js
@@ -6,8 +6,15 @@ import s from './styles.module.scss';
 const StudiosSidebar = ({ className, title, description }) => {
   return (
     <div className={s.content}>
-      <h1 className={s.title}>{title}</h1>
-      <RichText blocks={description} />
+      <div>
+        <h1 className={s.title}>{title}</h1>
+        <RichText blocks={description} />
+      </div>
+      <aside className={s.aside}>
+        <p>
+          Click on touch points to interact with different areas of the studio.
+        </p>
+      </aside>
     </div>
   );
 };
