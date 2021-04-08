c380d5b8bfbd5473bd7a051ab247b45b366d1e24
gvjacob <gvjacob@outlook.com>
2021-04-07 23:30:41 -0400

    scroll on hover

--- web/src/components/StudiosScene/index.js
+++ web/src/components/StudiosScene/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Image } from '@components';
 import s from './styles.module.scss';
 
 const StudiosScene = ({ className, scene }) => {
-  return <Image className={s.image} img={scene.image} />;
+  const ref = useRef();
+
+  useEffect(() => {
+    const containerEl = ref.current;
+
+    containerEl.onmousemove = ({ layerX }) => {
+      const width = containerEl.offsetWidth;
+      const relativeX = layerX / width;
+      const containerElScrollDifference =
+        containerEl.scrollWidth - containerEl.offsetWidth;
+
+      containerEl.scrollLeft = relativeX * containerElScrollDifference;
+    };
+  }, []);
+
+  return (
+    <div className={s.scene} ref={ref}>
+      <Image className={s.image} img={scene.image} />
+    </div>
+  );
 };
 
 export default StudiosScene;
