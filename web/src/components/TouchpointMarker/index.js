82b8c7d8fcade1d1c6b0363401b90a6f2a6c822f
gvjacob <gvjacob@outlook.com>
2021-04-12 20:46:24 -0400

    touchpoint markers

--- web/src/components/TouchpointMarker/index.js
+++ web/src/components/TouchpointMarker/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import cn from 'classnames';
 import s from './styles.module.scss';
 
-const TouchpointMarker = ({ className, style, number }) => {
+const TouchpointMarker = ({ className, style, number, onClick }) => {
   return (
-    <button className={cn(s.marker, className)} style={style}>
+    <button className={cn(s.marker, className)} style={style} onClick={onClick}>
       {number}
     </button>
   );
