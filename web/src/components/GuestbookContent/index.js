23e9aab801ae82da83c7112d6cd074532bb708cf
gvjacob <gvjacob@outlook.com>
2021-06-13 15:17:51 -0400

    case for empty reviews

--- web/src/components/GuestbookContent/index.js
+++ web/src/components/GuestbookContent/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import cn from 'classnames';
+import { If } from 'react-if';
+import isEmpty from 'lodash.isempty';
 
 import { GuestbookReviews, GuestbookSign, GuestbookCTAs } from '@components';
 import s from './styles.module.scss';
 
 const GuestbookContent = ({ className, guestbook }) => {
   const { reviews } = guestbook;
+  const withReviews = !isEmpty(reviews);
 
   return (
-    <div className={s.container}>
-      <GuestbookReviews reviews={reviews} />
+    <div className={cn(s.container, withReviews && s.withReviews)}>
+      <If condition={withReviews}>
+        <GuestbookReviews reviews={reviews} />
+      </If>
       <div className={s.signCtas}>
         <GuestbookSign shareConsentText={guestbook.share_consent_text} />
         <GuestbookCTAs
