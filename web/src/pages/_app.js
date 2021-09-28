435e3fbd973faacdcc8afbe9e46970bba50c0e54
Stephen Cronin <cronin4392@gmail.com>
2021-09-28 14:02:36 -0400

    More

--- web/src/pages/_app.js
+++ web/src/pages/_app.js
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import groq from 'groq';
+import { useRouter } from "next/router";
+import Script from 'next/script'
 
 import client from '@client';
 import { Footer, SEO } from '@components';
 import { GROQ } from '@utils/constants';
+import * as gtag from "../lib/gtag";
 import '../styles/index.scss';
 
 /**
@@ -11,10 +15,38 @@ import '../styles/index.scss';
 function App({ Component, pageProps, props }) {
   const { footer, siteOptions, navigation } = props;
   const { seo } = siteOptions;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
 
   return (
     <>
       <SEO {...seo} />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <Component
         {...pageProps}
